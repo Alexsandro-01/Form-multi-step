c1baa1afc5663e85d1e6cc74be7fa2867f6f702b
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-09 19:57:58 -0300

    feat: :sparkles: Botões para navegar entre os steps do form

---
+++ src/components/Boxbuttons.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Boxbuttons() {
+  return (
+    <div>
+      <button>
+        Go Back
+      </button>
+      <button>
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Boxbuttons;
