cb83b911438bc3da28dab2d6cd7572d5c54b123f
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-16 13:49:42 -0300

    style: :art: Formatação com espaços

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,35 +4,35 @@ import styles from '../styles/Sidebar.module.css';
 function Sidebar() {
   return (
     <section className={styles.sidebar}>
-          <div>
-            <span>1</span>
-            <div className={styles.text_step}>
-              <small>STEP 1</small>
-              <p>YOUR INFO</p>
-            </div>
-          </div>
-          <div>
-            <span>2</span>
-            <div className={styles.text_step}>
-              <small>STEP 2</small>
-              <p>SELECT PLAN</p>
-            </div>
-          </div>
-          <div>
-            <span>3</span>
-            <div className={styles.text_step}>
-              <small>STEP 3</small>
-              <p>ADD-ONS</p>
-            </div>
-          </div>
-          <div>
-            <span>4</span>
-            <div className={styles.text_step}>
-              <small>STEP 4</small>
-              <p>SUMMARY</p>
-            </div>
-          </div>
-        </section>
+      <div>
+        <span>1</span>
+        <div className={styles.text_step}>
+          <small>STEP 1</small>
+          <p>YOUR INFO</p>
+        </div>
+      </div>
+      <div>
+        <span>2</span>
+        <div className={styles.text_step}>
+          <small>STEP 2</small>
+          <p>SELECT PLAN</p>
+        </div>
+      </div>
+      <div>
+        <span>3</span>
+        <div className={styles.text_step}>
+          <small>STEP 3</small>
+          <p>ADD-ONS</p>
+        </div>
+      </div>
+      <div>
+        <span>4</span>
+        <div className={styles.text_step}>
+          <small>STEP 4</small>
+          <p>SUMMARY</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
