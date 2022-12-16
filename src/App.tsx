f389efc419da24c5b3c229b9431648022ae243f5
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-16 13:45:56 -0300

    feat: :sparkles: Adição da funcionalidade para se mover pelos steps do form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
-import styles from './styles/App.module.css';
-import StepOne from './components/StepOne';
-import Sidebar from './components/Sidebar';
+import React, { useState } from 'react';
 import Boxbuttons from './components/Boxbuttons';
+import Sidebar from './components/Sidebar';
+import StepOne from './components/StepOne';
+import StepTwo from './components/StepTwo';
+
+import styles from './styles/App.module.css';
 
 function App() {
+  const [step, setStep] = useState<number>(1);
+
+  const steps: Record<number, JSX.Element> = {
+    1: <StepOne />,
+    2: <StepTwo />
+  };
+
+  function handleStep(value: number, e: React.FormEvent | undefined) {
+    e?.preventDefault();
+
+    if (!steps[value]) return;
+
+    setStep(value);
+  }
   return (
-    <main>
-      <section className={styles.container}>
-        <Sidebar />
+  <main>
+    <section className={styles.container}>
+      <Sidebar />
 
-        <form>
-          <StepOne />
+      <form onSubmit={(e) => handleStep(step + 1, e)}>
+        {
+          steps[step]
+        }
 
-          <Boxbuttons />
-        </form>
-      </section>
-    </main>
+        <Boxbuttons
+          step={step}
+          handleStep={handleStep}
+        />
+      </form>
+    </section>
+  </main>
   );
 }
 
