2ff6a06fe5eea7fc2f9a059e7522bf05cd0f0e84
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-19 19:20:19 -0300

    feat: :sparkles: Adiciona validações para o step one

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Boxbuttons from './components/Boxbuttons';
 import Sidebar from './components/Sidebar';
 import StepOne from './components/StepOne';
+import StepThree from './components/StepThree';
 import StepTwo from './components/StepTwo';
 
 import styles from './styles/App.module.css';
 
+import AppContext from './context/AppContext';
+import { IPersonalInfo } from './interfaces/IContext';
+
 function App() {
   const [step, setStep] = useState<number>(1);
+  const ctx = useContext(AppContext);
 
   const steps: Record<number, JSX.Element> = {
     1: <StepOne />,
-    2: <StepTwo />
+    2: <StepTwo />,
+    3: <StepThree />
   };
+
+  function validations(value: number) {
+    const { name, email, phone } = ctx?.personalInfo as IPersonalInfo;
+
+    const phoneNumber = phone.replace(' ', '').replace('(', '').replace(')', '');
+    const validEmail = /\S+@\S+\.\S+/;
+
+    if (!name) {
+      ctx?.setFieldsValidations({
+        ...ctx.fieldesValidations,
+        nameError: 'This field is required'
+      });
+    } else if (!email) {
+      ctx?.setFieldsValidations({
+        ...ctx.fieldesValidations,
+        nameError: '',
+        emailError: 'This field is required'
+      });
+    } else if (!validEmail.test(email)) {
+      ctx?.setFieldsValidations({
+        ...ctx.fieldesValidations,
+        nameError: '',
+        emailError: 'Invalid email adress'
+      });
+    } else if (!phoneNumber) {
+      ctx?.setFieldsValidations({
+        nameError: '',
+        emailError: '',
+        phoneError: 'This field is required'
+      });
+    } else if (phoneNumber.length < 11) {
+      ctx?.setFieldsValidations({
+        nameError: '',
+        emailError: '',
+        phoneError: 'Invalid phone number'
+      });
+    } else {
+      ctx?.setFieldsValidations({
+        nameError: '',
+        emailError: '',
+        phoneError: ''
+      });
+      setStep(value);
+    }
+  }
 
   function handleStep(value: number, e: React.FormEvent | undefined) {
     e?.preventDefault();
 
     if (!steps[value]) return;
 
-    setStep(value);
+    validations(value);
   }
   return (
   <main>
